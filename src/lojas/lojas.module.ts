6d31a5a9ed4c99a278f1ea98fa1e393ec8a1a3aa
Lucas <lucashenrique4848@gmail.com>
2025-04-10 16:53:02 -0300

    integração da API do viacep ao projeto

--- src/lojas/lojas.module.ts
+++ src/lojas/lojas.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Store } from "./entities/loja.entity";
 import { LojasService } from "./lojas.service";
 import { LojasController } from "./lojas.controller";
+import { ViaCepService } from "src/via-cep/via-cep.service";
+import { MapsService } from "src/maps/maps.service";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Store])],
-  providers: [LojasService],
+  imports: [TypeOrmModule.forFeature([Store]), HttpModule],
+  providers: [LojasService, ViaCepService, MapsService],
   controllers: [LojasController],
 })
 export class LojasModule implements OnModuleInit {
