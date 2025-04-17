6d4f10757016223290f73d2bee52e45e146a9437
Lucas <lucashenrique4848@gmail.com>
2025-04-17 14:45:18 -0300

    corrige erro de importação de módulo inexsistente

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from "./app.service";
 import { LojasModule } from "./lojas/lojas.module";
 import { FreteModule } from "./frete/frete.module";
 import { MapsModule } from "./maps/maps.module";
-import { CorreiosModule } from "./correios/correios.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Store } from "./lojas/entities/loja.entity";
 import { ConfigModule } from "@nestjs/config";
@@ -21,7 +20,6 @@ import { ConfigModule } from "@nestjs/config";
     LojasModule,
     FreteModule,
     MapsModule,
-    CorreiosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
